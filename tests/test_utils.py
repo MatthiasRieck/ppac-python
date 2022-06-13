4927b9999540723cb5d67e4461c348e37e864e18
Matthias Rieck <Matthias.Rieck@icloud.com>
2022-06-13 12:29:55 +0200

    Resolve Pylint Issue Testcases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,11 +6,11 @@ from ppac.utils import datetime_to_str, str_to_datetime
 
 class TestDatetimeStrConversion(TestCase):
     def test_to_string(self):
-        d = datetime(2022, 5, 3, 12, 34, 44)
-        txt = datetime_to_str(d)
+        date_time = datetime(2022, 5, 3, 12, 34, 44)
+        txt = datetime_to_str(date_time)
         self.assertEqual(txt, '2022-05-03T12:34:44')
 
     def test_from_string(self):
         txt = '2022-05-03T12:34:44'
-        d = str_to_datetime(txt)
-        self.assertEqual(d, datetime(2022, 5, 3, 12, 34, 44))
+        date_time = str_to_datetime(txt)
+        self.assertEqual(date_time, datetime(2022, 5, 3, 12, 34, 44))
